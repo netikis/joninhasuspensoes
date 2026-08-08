7c201a348343e1f8831bd27228a5f0383e86426a
J.neto <netikis@gmail.com>
2026-08-08 11:53:55 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Joninha Suspensões — PWA com atualização automática (PC + celular) */
-var CACHE = 'joninha-suspensoes-v18';
+var CACHE = 'joninha-suspensoes-v19';
 var ASSETS = [
   './',
   './index.html',
